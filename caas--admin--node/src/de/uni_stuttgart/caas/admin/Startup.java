f03464596ae895b4bc39da86c165d026e703c0a6
Alexander Gessler <alexander.gessler@gmx.net>
2013-06-04 13:55:56 +0200

    Init admin node and cache node project stubs.

---
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/Startup.java
@@ -0,0 +1,13 @@
+package de.uni_stuttgart.caas.admin;
+
+public class Startup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ caas--cache--node/src/de/uni_stuttgart/caas/cache/Startup.java
@@ -0,0 +1,13 @@
+package de.uni_stuttgart.caas.cache;
+
+public class Startup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
