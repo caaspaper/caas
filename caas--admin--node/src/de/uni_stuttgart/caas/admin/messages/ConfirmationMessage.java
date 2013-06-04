b17ae15699cf90433cd24294e3fdd31a8ce0099b
Alexander Gessler <alexander.gessler@gmx.net>
2013-06-04 15:51:00 +0200

    Draft ConfirmationMessage

--- caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/ConfirmationMessage.java
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/ConfirmationMessage.java
@@ -1,5 +1,31 @@
 package de.uni_stuttgart.caas.admin.messages;
 
-public class ConfirmationMessage {
+/**
+ * Encapsulates a confirmation message that contains a status code, which
+ * indicates whether another operation was successful or not.
+ */
+public class ConfirmationMessage implements IMessage {
 
+	/**
+	 * Status code with semantics that 0 means success and non-0 indicates
+	 * failure (the exact interpretation of which is up to the user)
+	 */
+	public final int STATUS_CODE;
+
+	/**
+	 * Optional message that further describes the status. This value may be
+	 * null.
+	 */
+	public final String MESSAGE;
+
+	
+	public ConfirmationMessage(int statusCode, String message) {
+		STATUS_CODE = statusCode;
+		MESSAGE = message;
+	}
+
+	@Override
+	public MessageType GetMessage() {
+		return MessageType.CONFIRM;
+	}
 }
