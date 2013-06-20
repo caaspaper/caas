70cf22bbc71ed2dfddfb78ec6a27c030cce8fadb
Simon Hanna <simon.hanna@jesus.de>
2013-06-20 16:20:29 +0200

    basic activate message

---
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/ActivateMessage.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.uni_stuttgart.caas.admin.messages;
+
+/**
+ * @author Alexander C. Gessler, Ashley M. Smith, Simon Hanna
+ *
+ */
+public class ActivateMessage implements IMessage{
+
+	/**
+	 * @see de.uni_stuttgart.caas.admin.messages.IMessage#GetMessage()
+	 */
+	@Override
+	public MessageType GetMessage() {
+		
+		return MessageType.ACTIVATE;
+	}
+
+}
