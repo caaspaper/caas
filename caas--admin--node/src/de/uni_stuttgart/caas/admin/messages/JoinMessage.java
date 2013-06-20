acaf3cf7d85439761ba6ee1f812120f646a959d6
Simon Hanna <simon.hanna@jesus.de>
2013-06-20 16:10:12 +0200

    implemented Join message

--- caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/JoinMessage.java
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/JoinMessage.java
@@ -1,5 +1,14 @@
 package de.uni_stuttgart.caas.admin.messages;
 
-public class JoinMessage {
+public class JoinMessage implements IMessage {
+
+	/**
+	 * @see de.uni_stuttgart.caas.admin.messages.IMessage#GetMessage()
+	 */
+	@Override
+	public MessageType GetMessage() {
+		
+		return MessageType.JOIN;
+	}
 
 }
