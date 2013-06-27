37931c7c1f064cac73a23d19f6b9f1b815b927b7
Simon Hanna <simon.hanna@jesus.de>
2013-06-27 13:24:00 +0200

    basic implementation of AddToGridMessage

--- caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/AddToGridMessage.java
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/messages/AddToGridMessage.java
@@ -1,5 +1,35 @@
 package de.uni_stuttgart.caas.admin.messages;
 
-public class AddToGridMessage {
+import java.util.Collection;
+import delaunay.Segment;
 
+/**
+ * Message sent to cache node containing information about neighboring nodes
+ */
+public class AddToGridMessage implements IMessage{
+
+	private Collection<Segment> neighoringNodes;
+
+	/**
+	 * Constructor of message
+	 * 
+	 * @param neighboringNodes
+	 *            a collection of neighboring nodes
+	 */
+	public AddToGridMessage(Collection<Segment> neighboringNodes) {
+		this.neighoringNodes = neighboringNodes;
+	}
+
+	/**
+	 * Get the neighboring nodes
+	 * @return the neighboring nodes
+	 */
+	public Collection<Segment> getNeighboringNodes() {
+		return neighoringNodes;
+	}
+
+	@Override
+	public MessageType GetMessage() {
+		return MessageType.ADD_TO_GRID;
+	}
 }
