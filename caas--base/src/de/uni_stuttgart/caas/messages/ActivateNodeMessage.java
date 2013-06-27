846c94b75c3285447e78466dd7ad7de9ccaf598f
Simon Hanna <simon.hanna@jesus.de>
2013-06-27 14:17:23 +0200

    removed duplicate Class

--- caas--base/src/de/uni_stuttgart/caas/messages/ActivateNodeMessage.java
+++ caas--base/src/de/uni_stuttgart/caas/messages/ActivateNodeMessage.java
@@ -1,5 +1,17 @@
 package de.uni_stuttgart.caas.messages;
 
-public class ActivateNodeMessage {
+/**
+ * @author Alexander C. Gessler, Ashley M. Smith, Simon Hanna
+ *
+ */
+public class ActivateNodeMessage implements IMessage {
 
+	/**
+	 * @see de.uni_stuttgart.caas.admin.messages.IMessage#GetMessage()
+	 */
+	@Override
+	public MessageType GetMessage() {
+		
+		return MessageType.ACTIVATE;
+	}
 }
