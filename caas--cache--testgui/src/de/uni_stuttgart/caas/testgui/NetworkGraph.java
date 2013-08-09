43da2c3a8863f653d2395121c3dba471c69c7deb
Simon Hanna <simon.hanna@jesus.de>
2013-08-09 11:30:06 +0200

    NetworkGraph now opens with a default size (hopefully enough for most cases)

--- caas--cache--testgui/src/de/uni_stuttgart/caas/testgui/NetworkGraph.java
+++ caas--cache--testgui/src/de/uni_stuttgart/caas/testgui/NetworkGraph.java
@@ -41,6 +41,7 @@ public class NetworkGraph extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setSize(1000, 1000);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		drawer = new TriangulationDrawer(800, 800);
