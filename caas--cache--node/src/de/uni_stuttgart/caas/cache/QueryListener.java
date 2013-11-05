46caa6ecc45ea395f634971bd097514b16e27741
Simon Hanna <simon.hanna@jesus.de>
2013-11-05 15:27:46 +0100

    implement a Server that listens for connections and creats Queries

---
+++ caas--cache--node/src/de/uni_stuttgart/caas/cache/QueryListener.java
@@ -0,0 +1,90 @@
+package de.uni_stuttgart.caas.cache;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class QueryListener implements Runnable {
+
+	private CacheNode cacheNode;
+	private ServerSocket serverSocket;
+	private boolean isRunning;
+	
+	public QueryListener(CacheNode cacheNode, int port) {
+		
+		this.cacheNode = cacheNode;
+		try {
+			serverSocket = new ServerSocket(port);
+			serverSocket.setSoTimeout(1000);
+			isRunning = true;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		Socket clientSocket;
+		while (isRunning) {
+			
+			try {
+				clientSocket = serverSocket.accept();
+				new Thread(new ListenerThread(cacheNode, clientSocket)).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Stops the server
+	 */
+	public void stop() {
+		isRunning = false;
+	}
+	
+	
+	/**
+	 * Accepts new queries
+	 * 
+	 */
+	private class ListenerThread implements Runnable{
+		
+		private CacheNode cacheNode;
+		private Socket clientSocket;
+		
+		public ListenerThread(CacheNode cacheNode, Socket clientSocket) {
+			this.cacheNode = cacheNode;
+			this.clientSocket = clientSocket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					if (!line.equals("STOP")) {
+						cacheNode.processIncomingQueryToAdaptItToNetwork(null);
+					} else {
+						break;
+					}
+				}
+				reader.close();
+				clientSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
