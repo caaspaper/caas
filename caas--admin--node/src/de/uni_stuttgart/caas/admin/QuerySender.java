188d6f3427788f0dab886e3a3cf412288e281f92
Alexander Gessler <alexander.gessler@gmx.net>
2013-12-01 15:04:20 +0100

    bugfix query sending if count-queries > count-nodes

--- caas--admin--node/src/de/uni_stuttgart/caas/admin/QuerySender.java
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/QuerySender.java
@@ -31,7 +31,8 @@ public class QuerySender {
 	public static void generateRandomQuery(String ip, int port, LogSender logger) {
 		QueryReceiver receiver = new QueryReceiver(logger, 1);
 		(new Thread(receiver)).start();
-		sendQuery(new QueryMessage(new LocationOfNode(500, 500), ip, receiver.getPort(), new InetSocketAddress(ip, port), UUID.randomUUID().getLeastSignificantBits()), receiver);
+		sendQuery(new QueryMessage(new LocationOfNode(500, 500), ip, receiver.getPort(), new InetSocketAddress(ip, port), UUID.randomUUID()
+				.getLeastSignificantBits()), receiver);
 	}
 
 	public static void generateUniformlyDistributedQueries(int numOfQueriesPerNode, Map<InetSocketAddress, NodeInfo> nodes, LogSender logger) {
@@ -48,7 +49,7 @@ public class QuerySender {
 		long id = 0;
 		for (Entry<InetSocketAddress, NodeInfo> e : nodes.entrySet()) {
 			for (int i = 0; i < numOfQueriesPerNode; ++i) {
-				final QueryMessage m = new QueryMessage(e.getValue().getLocationOfNode(), ip, port, addresses.get(counter), ++id);
+				final QueryMessage m = new QueryMessage(e.getValue().getLocationOfNode(), ip, port, addresses.get(counter++ % addresses.size()), ++id);
 				executor.execute(new Runnable() {
 
 					@Override
@@ -56,14 +57,12 @@ public class QuerySender {
 						sendQuery(m, receiver);
 					}
 				});
-				++counter;
 			}
-			counter = 0;
 		}
 	}
-	
-	public static void generateQueriesWithRandomHotspotOneEntryPoint (int numOfQueries, Grid g, LogSender logger) {
-		
+
+	public static void generateQueriesWithRandomHotspotOneEntryPoint(int numOfQueries, Grid g, LogSender logger) {
+
 		int randomNum = (int) (Math.random() * g.getConnectedNodes().size());
 		int counter = 0;
 		NodeInfo randomNode = null;
@@ -77,17 +76,17 @@ public class QuerySender {
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		final QueryReceiver receiver = new QueryReceiver(logger, numOfQueries);
 		(new Thread(receiver)).start();
-		
+
 		String ip = receiver.getHost();
 		int port = receiver.getPort();
-		
+
 		ArrayList<NodeInfo> neighbors = new ArrayList<>(g.getNeighborInfo(randomNode.NODE_ADDRESS));
 		neighbors.add(randomNode);
 		for (int i = 0; i < numOfQueries; ++i) {
 			randomNum = (int) (Math.random() * neighbors.size());
 			final QueryMessage m = new QueryMessage(neighbors.get(randomNum).getLocationOfNode(), ip, port, randomNode.ADDRESS_FOR_CACHENODE_QUERYLISTENER, i);
 			executor.execute(new Runnable() {
-				
+
 				@Override
 				public void run() {
 					sendQuery(m, receiver);
@@ -95,17 +94,17 @@ public class QuerySender {
 			});
 		}
 	}
-	
-	public static void generateUniformlyDistributedQueriesEnteringAtOneLocation(int numOfQueriesPerNode,
-			Map<InetSocketAddress, NodeInfo> nodes, LogSender logger) {
-	
+
+	public static void generateUniformlyDistributedQueriesEnteringAtOneLocation(int numOfQueriesPerNode, Map<InetSocketAddress, NodeInfo> nodes,
+			LogSender logger) {
+
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		final QueryReceiver receiver = new QueryReceiver(logger, numOfQueriesPerNode * nodes.size());
 		(new Thread(receiver)).start();
-		
+
 		String ip = receiver.getHost();
 		int port = receiver.getPort();
-		
+
 		int randomNum = (int) (Math.random() * nodes.size());
 		InetSocketAddress randomEntryNode = null;
 		for (Entry<InetSocketAddress, NodeInfo> e : nodes.entrySet()) {
@@ -189,7 +188,7 @@ class QueryReceiver implements Runnable {
 		}
 		queries = new ConcurrentHashMap<>(numOfQueriesSent);
 	}
-	
+
 	public void waitForQuery(QueryMessage m, Date d) {
 		queries.putIfAbsent(m.ID, new de.uni_stuttgart.caas.base.QueryLog(d, m.ID));
 	}
@@ -197,7 +196,7 @@ class QueryReceiver implements Runnable {
 	public int getPort() {
 		return serverSocket.getLocalPort();
 	}
-	
+
 	public String getHost() {
 		return serverSocket.getInetAddress().getHostAddress();
 	}
@@ -235,7 +234,7 @@ class QueryReceiver implements Runnable {
 								QueryLog l = queries.get(r.ID);
 								l.finishQuery(d, r.getDebuggingInfo().split("-"));
 								synchronized (writer) {
-									l.writeToFile(writer);	
+									l.writeToFile(writer);
 								}
 							} else {
 								logger.write("Client didn't receive QueryResult but some other Object");
