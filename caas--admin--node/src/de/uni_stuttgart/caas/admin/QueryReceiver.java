b6708a0bd61633f6c63c2670ae5a57d0e7d77ecd
Alexander Gessler <alexander.gessler@gmx.net>
2013-12-08 14:24:41 +0100

    Refactor: Cleanup & document QueryReceiver API and promote it to a package-level class. Hardcode elb for now.

---
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/QueryReceiver.java
@@ -0,0 +1,199 @@
+package de.uni_stuttgart.caas.admin;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import de.uni_stuttgart.caas.base.LogSender;
+import de.uni_stuttgart.caas.base.QueryLog;
+import de.uni_stuttgart.caas.messages.QueryMessage;
+import de.uni_stuttgart.caas.messages.QueryResult;
+
+public final class QueryReceiver {
+
+	private final ServerSocket serverSocket;
+	private final LogSender logger;
+	private final CountDownLatch syncPoint;
+	private final ConcurrentHashMap<Long, de.uni_stuttgart.caas.base.QueryLog> queries;
+	private final BufferedWriter writer;
+	private volatile boolean stop = false;
+
+	private long times[];
+	private final Thread thread;
+
+	public QueryReceiver(LogSender _logger, int numOfQueriesSent, boolean enableLogging) throws IOException {
+		queries = new ConcurrentHashMap<>(numOfQueriesSent);
+		times = new long[numOfQueriesSent];
+		logger = _logger;
+		syncPoint = new CountDownLatch(numOfQueriesSent);
+
+		// extra-long backlog to make sure we can process incoming queries
+		serverSocket = new ServerSocket(0, 256);
+
+		BufferedWriter buff = null;
+
+		if (enableLogging) {
+			try {
+				buff = new BufferedWriter(new FileWriter(new File("/tmp/queryStats.csv")));
+			} catch (IOException e) {
+				try {
+					buff = new BufferedWriter(new FileWriter(new File("queryStats.csv")));
+				} catch (IOException e1) {
+					e.printStackTrace();
+					throw e1;
+				}
+
+			}
+
+			try {
+				buff.write("ID,queryTime(ms),hopCount,path\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		writer = buff;
+
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				acceptor();
+			}
+		});
+		thread.start();
+	}
+
+	/**
+	 * Await reception of all query responses previously registered using
+	 * expectQueryResponse().
+	 * 
+	 * Additionally, a 10s timeout is used to cut out any failed queries. After
+	 * join() returns, getTimes() can be used to get an array of millisecond
+	 * latencies for every query for which a response was received.
+	 * 
+	 * @return Number of queries for which a response was not received within
+	 *         the time.
+	 */
+	public long join() {
+		// if some queries fail, we cut them off with a timeout
+		try {
+			syncPoint.await(10, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long missing = 0;
+		synchronized (syncPoint) {
+			missing = syncPoint.getCount();
+			times = Arrays.copyOf(times, (int) (times.length - missing));
+
+			thread.interrupt();
+			stop = true;
+		}
+
+		try {
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return missing;
+	}
+
+	/** Register a query with the QueryReceiver to enable tracking for it */
+	public void expectQueryResponse(QueryMessage m, long time) {
+		queries.putIfAbsent(m.ID, new QueryLog(time, m.ID));
+	}
+
+	/** Get the local port that we listen on */
+	public int getPort() {
+		return serverSocket.getLocalPort();
+	}
+
+	public String getHost() {
+		return serverSocket.getInetAddress().getHostAddress();
+	}
+
+	/** @note Only valid after join() returns */
+	public long[] GetTimes() {
+		assert stop;
+		return times;
+	}
+
+	private void acceptor() {
+
+		while (!thread.isInterrupted()) {
+			try {
+				final Socket s = serverSocket.accept();
+				new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						ObjectInputStream in;
+						try {
+							in = new ObjectInputStream(s.getInputStream());
+						} catch (IOException e) {
+							e.printStackTrace();
+							return;
+						}
+						Object o;
+						try {
+							o = in.readObject();
+						} catch (ClassNotFoundException e) {
+							e.printStackTrace();
+							return;
+						} catch (IOException e) {
+							e.printStackTrace();
+							return;
+						}
+						long time = System.nanoTime();
+						if (o instanceof QueryResult) {
+
+							logger.write("received answer to a query");
+							QueryResult r = (QueryResult) o;
+							QueryLog l = queries.get(r.ID);
+							l.finishQuery(time, r.getDebuggingInfo().split("-"));
+
+							// TODO: lock time is potentially too long.
+							synchronized (syncPoint) {
+								// if the stop event happened, no further times
+								// may be recorded
+								if (stop) {
+									return;
+								}
+
+								if (writer != null) {
+									l.writeToFile(writer);
+								}
+								long l1 = syncPoint.getCount();
+								times[(int) (l1 - 1)] = l.getTransitTime() / 1000000;
+
+								if (l1 % 100 == 0 || l1 < 50) {
+									System.out.println(l1);
+								}
+
+								syncPoint.countDown();
+							}
+						} else {
+							logger.write("didn't receive QueryResult object, do not now what to do");
+						}
+
+						try {
+							in.close();
+							s.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- caas--admin--node/src/de/uni_stuttgart/caas/admin/QuerySender.java
+++ caas--admin--node/src/de/uni_stuttgart/caas/admin/QuerySender.java
@@ -1,73 +1,58 @@
 package de.uni_stuttgart.caas.admin;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.Inet4Address;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 import de.uni_stuttgart.caas.base.LocationOfNode;
 import de.uni_stuttgart.caas.base.LogSender;
 import de.uni_stuttgart.caas.base.NodeInfo;
-import de.uni_stuttgart.caas.base.QueryLog;
 import de.uni_stuttgart.caas.messages.QueryMessage;
-import de.uni_stuttgart.caas.messages.QueryResult;
 
 public class QuerySender {
 
 	// seconds
 	public static final int totalBenchmarkTime = 10;
 
-	public static void generateRandomQuery(String ip, int port, LogSender logger) {
-		QueryReceiver receiver = new QueryReceiver(logger, 1);
-		(new Thread(receiver)).start();
-		sendQuery(new QueryMessage(new LocationOfNode(500, 500), ip, receiver.getPort(), new InetSocketAddress(ip, port), UUID.randomUUID()
-				.getLeastSignificantBits()), receiver);
-	}
-
-	public static void generateDistributedQueries(final int numOfQueriesPerNodeAndSecond, Map<InetSocketAddress, NodeInfo> nodes, LogSender logger,
+	public static void generateDistributedQueries(final int numOfQueriesPerNodeAndSecond, final Map<InetSocketAddress, NodeInfo> nodes, final LogSender logger,
 			final boolean uniform) {
+
+		// TODO: migrate from sysout to logger - right now sysout is used
+		// because logging is purposedly turned off during benchmarks, but we
+		// would still like to see benchmark results.
 
 		final CountDownLatch count = new CountDownLatch(nodes.size());
 		final int perNode = totalBenchmarkTime * numOfQueriesPerNodeAndSecond;
 		final int qcount = perNode * nodes.size();
 
-		final QueryReceiver receiver = new QueryReceiver(logger, qcount);
-		(new Thread(receiver)).start();
-		
-		String tempHost = "";
+		final QueryReceiver receiver;
 		try {
-			tempHost = Inet4Address.getLocalHost().getHostAddress();
-		} catch (UnknownHostException e2) {
+			receiver = new QueryReceiver(logger, qcount, false);
+		} catch (IOException e2) {
+			System.out.println("benchmark: failed to construct QueryReceiver");
 			e2.printStackTrace();
-			assert false;
 			return;
-		};
-		
+		}
+
+		// TODO: hardcoded for now because getHostName() gives incorrect value
+		// on the elb, thus causing queries to be not received.
+		String tempHost = "elb"; /*
+								 * try { tempHost =
+								 * Inet4Address.getLocalHost().getHostName(); }
+								 * catch (UnknownHostException e2) {
+								 * e2.printStackTrace(); assert false; return;
+								 * };
+								 */
+
 		final String localHost = tempHost;
 		final int port = receiver.getPort();
 
 		// always place the hotspot at a fixed position in the grid as to avoid
-		// another random variable in the game. Do not place it in the center, 
+		// another random variable in the game. Do not place it in the center,
 		// as this would minimize the number of hops and is thus unfair with
 		// respect to the uniform case.
 		final LocationOfNode hotspot = new LocationOfNode(Grid.MAX_GRID_INDEX / 5, Grid.MAX_GRID_INDEX / 5);
@@ -136,40 +121,43 @@ public class QuerySender {
 			assert false;
 		}
 
-		receiver.join();
-		System.out.println("benchmark completed, " + qcount + " queries over " + totalBenchmarkTime + "s and " + nodes.size()
-				+ " nodes. " + perNode + " queries per node, " + (uniform ? "uniform" : "gaussian") + " distribution");
-		
+		final long missing = receiver.join();
+		System.out.println("benchmark completed, " + qcount + " queries over " + totalBenchmarkTime + "s and " + nodes.size() + " nodes. " + perNode
+				+ " queries per node, " + (uniform ? "uniform" : "gaussian") + " distribution");
+
+		if (missing > 0) {
+			System.out.println(missing + " queries never returned");
+		}
+
 		// compute mean and median of all timings
 		long[] times = receiver.GetTimes();
 		Arrays.sort(times);
-		
+
 		// take the upper 95% (P95)
-		times = Arrays.copyOfRange(times, 0, (int)(times.length * 0.95) );
-		
+		times = Arrays.copyOfRange(times, 0, (int) (times.length * 0.95));
+
 		assert times.length > 0;
-		final long median = times[times.length/2];
-		
+		final long median = times[times.length / 2];
+
 		double mean = 0.0;
-		for(long l: times) {
-			mean += (double)l;
+		for (long l : times) {
+			mean += (double) l;
 		}
 		mean /= times.length;
-		
+
 		System.out.println("mean: " + mean + "ms");
 		System.out.println("median: " + median + "ms");
 	}
 
-
-	public static void sendQuery(QueryMessage m, QueryReceiver r) {
+	private static void sendQuery(QueryMessage m, QueryReceiver r) {
 		try {
 			Socket s = new Socket(m.ENTRY_LOCATION.getHostString(), m.ENTRY_LOCATION.getPort());
 			ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
-			r.waitForQuery(m, System.nanoTime());
+			r.expectQueryResponse(m, System.nanoTime());
 			out.writeObject(m);
 
 			// bad idea, makes our benchmarking excessively slow - rather GC
-			// collect the socket.s
+			// collect the socket.
 			// Thread.sleep(500);
 			// out.close();
 			// s.close();
@@ -179,159 +167,5 @@ public class QuerySender {
 		// } catch (InterruptedException e) {
 		// e.printStackTrace();
 		// }
-	}
-
-	public static void main(String[] args) {
-		generateRandomQuery("localhost", 40048, new LogSender(null, false, false, true));
-	}
-}
-
-class QueryReceiver implements Runnable {
-
-	private ServerSocket serverSocket;
-	private Thread t;
-	private LogSender logger;
-	private CountDownLatch syncPoint;
-	private ConcurrentHashMap<Long, de.uni_stuttgart.caas.base.QueryLog> queries;
-	private BufferedWriter writer;
-	
-	private final long times[];
-
-	public QueryReceiver(LogSender logger, int numOfQueriesSent) {
-		times = new long[numOfQueriesSent];
-		this.logger = logger;
-		syncPoint = new CountDownLatch(numOfQueriesSent);
-		try {
-			serverSocket = new ServerSocket(0);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			writer = new BufferedWriter(new FileWriter(new File("/tmp/queryStats.csv")));
-		} catch (IOException e) {
-			try {
-				writer = new BufferedWriter(new FileWriter(new File("queryStats.csv")));
-			} catch (IOException e1) {
-				e.printStackTrace();
-				e1.printStackTrace();
-			}
-
-		}
-		assert writer != null;
-		try {
-			writer.write("ID,queryTime(ms),hopCount,path\n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		queries = new ConcurrentHashMap<>(numOfQueriesSent);
-	}
-
-	public void join() {
-		try {
-			syncPoint.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		try {
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void waitForQuery(QueryMessage m, long time) {
-		queries.putIfAbsent(m.ID, new de.uni_stuttgart.caas.base.QueryLog(time, m.ID));
-	}
-
-	public int getPort() {
-		return serverSocket.getLocalPort();
-	}
-
-	public String getHost() {
-		return serverSocket.getInetAddress().getHostAddress();
-	}
-
-	public int stopAndGetNumOfOpenQueries() {
-		t.interrupt();
-		try {
-			t.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return (int) syncPoint.getCount();
-	}
-	
-	public long[] GetTimes() {
-		return times;
-	}
-
-	@Override
-	public void run() {
-		t = Thread.currentThread();
-		
-		while (!t.isInterrupted()) {
-			try {
-				final Socket s = serverSocket.accept();
-
-				Thread t = new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						ObjectInputStream in;
-						try {
-							in = new ObjectInputStream(s.getInputStream());
-						} catch (IOException e) {
-							e.printStackTrace();
-							return;
-						}
-						Object o;
-						try {
-							o = in.readObject();
-						} catch (ClassNotFoundException e) {
-							e.printStackTrace();
-							return;
-						} catch (IOException e) {
-							e.printStackTrace();
-							return;
-						}
-						long time = System.nanoTime();
-						if (o instanceof QueryResult) {
-
-							logger.write("Client received answer to a query");
-							QueryResult r = (QueryResult) o;
-							QueryLog l = queries.get(r.ID);
-							l.finishQuery(time, r.getDebuggingInfo().split("-"));
-							
-							synchronized(syncPoint) { // TODO: too long.
-								//l.writeToFile(writer);
-								
-								
-								long l1 = syncPoint.getCount();
-								times[(int) (l1 - 1)] = l.getTransitTime() / 1000000;
-								
-								syncPoint.countDown();
-								
-								if (l1 % 100 == 0 || l1 < 50) {
-									System.out.println(l1);
-								}
-							}
-						} else {
-							logger.write("Client didn't receive QueryResult but some other Object");
-						}
-						try {
-							in.close();
-							s.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				});
-				t.start();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-		}
 	}
 }
